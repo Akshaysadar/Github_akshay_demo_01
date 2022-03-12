6cf9834caeea38f64857460ec76ee9063f277e1f
akshaysadar <akshaysadar007@gmail.com>
2022-03-12 21:49:39 +0530

    Akshay commit to the capita innetbanking

---
+++ src/test/java/Org/ventura/Capita/inetbanking/AppTest.java
@@ -0,0 +1,20 @@
+package Org.ventura.Capita.inetbanking;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
